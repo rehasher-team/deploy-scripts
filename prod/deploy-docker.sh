e9412f8461d3b447e274d2774511bbb33d345c27
정석호 <nodejsdeveloper@kakao.com>
2025-05-04 17:39:18 +0900

    Create deploy-docker.sh

---
+++ prod/deploy-docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+yum update -y
+
+dnf update -y
+
+dnf install -y docker
+
+sudo systemctl enable docker
+
+sudo systemctl start docker
+
+usermod -a -G docker ec2-user
+
+yum install -y awscli
+
+AWS_REGION="ap-northeast-2"
+ECR_REPO_NAME="rehasher-team/rehash-backend"
+IMAGE_TAG="latest"
+ACCOUNT_ID="215626476886"
+IMAGE_URI="$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME:$IMAGE_TAG"
+
+aws ecr get-login-password --region $AWS_REGION | \
+  docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+docker pull $IMAGE_URI
+
+# docker rm -f rehash-backend || true
+# # 7. 포트 80 바인딩 + 컨테이너 실행 (root 권한)
+# docker run -d \
+#   --name rehash-backend \
+#   -p 80:3000 \
+#   --restart always \
+#   $IMAGE_URI
