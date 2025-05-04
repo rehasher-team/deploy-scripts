9a01ce952e60a57172f80fe64be0a32e054765be
정석호 <nodejsdeveloper@kakao.com>
2025-05-04 18:16:46 +0900

    Update health-server.js

--- prod/health-server.js
+++ prod/health-server.js
@@ -10,4 +10,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => console.log(`Server is running at http://localhost:${PORT}`));
+server.listen(3000, () => console.log(`Server is running at http://localhost:3000`));
