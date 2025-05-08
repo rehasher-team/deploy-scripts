9846b2a0c725f12aaab8f6670483cf1910b6d979
정석호 <nodejsdeveloper@kakao.com>
2025-05-08 19:37:40 +0900

    Update deploy-live.sh

--- deploy-live.sh
+++ deploy-live.sh
@@ -24,10 +24,10 @@ IMAGE_URI="$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME:$IMAGE_T
 aws ecr get-login-password --region $AWS_REGION | \
   docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-docker pull $IMAGE_URI
-
 docker rm -f rehash-backend || true
 # 7. 포트 80 바인딩 + 컨테이너 실행 (root 권한)
+
+docker pull $IMAGE_URI
 
 cd /home/ec2-user
 
